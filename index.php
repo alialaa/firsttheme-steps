dd1c279d112438a8a993b0535149b1caf5958466
Azaltarun <44315438+Azaltarun@users.noreply.github.com>
2018-10-21 13:31:41 +0530

    Setting Up Development Environment

---
+++ index.php
@@ -0,0 +1,14 @@
+<<!DOCTYPE html>
+<html lan="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+</head>
+<body>
+
+efneln
+    
+</body>
+</html>
