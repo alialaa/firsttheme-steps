a3992706a47982b65151626a0fe1446fafc05966
Ankit Jindal <akjaph@gmail.com>
2018-10-25 03:46:59 +0530

    compressing-images-and-copying-files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,7 +3,8 @@ import yargs from 'yargs';
 import sass from 'gulp-sass';
 import cleanCss from 'gulp-clean-css';
 import gulpif from 'gulp-if';
-import sourcemaps from 'gulp-sourcemaps'
+import sourcemaps from 'gulp-sourcemaps';
+import imagemin from 'gulp-imagemin';
 
 const PRODUCTION = yargs.argv.prod;
 
@@ -11,6 +12,14 @@ const paths = {
 	styles: {
 		src: ['src/assets/scss/bundle.scss','src/assets/scss/admin.scss'],
 		dest: 'dist/assets/css'
+	},
+	images: {
+		src: 'src/assets/images/**/*.{jpg,jpeg,png,svg,gif}',
+		dest: 'dist/assets/images'
+	},
+	other: {
+		src: ['src/assets/**/*','!src/assets/{images,js,scss}', '!src/assets/{images,js,scss}/**/*'],
+		dest: 'dist/assets'
 	}
 }
 
@@ -23,8 +32,19 @@ export const styles = (done) => {
 		.pipe(gulp.dest(paths.styles.dest));
 }
 
+export const images = () => {
+	return gulp.src(paths.images.src)
+		.pipe(gulpif(PRODUCTION, imagemin()))
+		.pipe(gulp.dest(paths.images.dest));
+}
+
 export const watch = () => {
 	gulp.watch('src/assets/scss/**/*.scss', styles);
 } 
 
+
+export const copy = () => {
+	return gulp.src(paths.other.src)
+		.pipe(gulp.dest(paths.other.dest));
+}
 //export default hello;
