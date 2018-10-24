181c159467dddf7a39b8166dde572bee42190814
Ankit Jindal <akjaph@gmail.com>
2018-10-25 03:00:37 +0530

    creating-a-gulp-task-for-styles-cont

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,14 +3,28 @@ import yargs from 'yargs';
 import sass from 'gulp-sass';
 import cleanCss from 'gulp-clean-css';
 import gulpif from 'gulp-if';
+import sourcemaps from 'gulp-sourcemaps'
 
 const PRODUCTION = yargs.argv.prod;
 
+const paths = {
+	styles: {
+		src: ['src/assets/scss/bundle.scss','src/assets/scss/admin.scss'],
+		dest: 'dist/assets/css'
+	}
+}
+
 export const styles = (done) => {
-	return gulp.src('src/assets/scss/bundle.scss')
+	return gulp.src(paths.styles.src)
+		.pipe(gulpif(!PRODUCTION, sourcemaps.init()))
 		.pipe(sass().on('error',sass.logError))
 		.pipe(gulpif(PRODUCTION, cleanCss({compatibility:'ie8'})))
-		.pipe(gulp.dest('dist/assets/css'));
+		.pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+		.pipe(gulp.dest(paths.styles.dest));
 }
+
+export const watch = () => {
+	gulp.watch('src/assets/scss/**/*.scss', styles);
+} 
 
 //export default hello;
--- lib/enqueue-assets.php
+++ lib/enqueue-assets.php
@@ -5,3 +5,10 @@ function firsttheme_assets() {
 }
 
 add_action('wp_enqueue_scripts','firsttheme_assets');
+
+
+function firsttheme_admin_assets() {
+	wp_enqueue_style( 'firsttheme-admin-stylesheet', get_template_directory_uri() . '/dist/assets/css/admin.css', array(), '1.0.0', 'all' );
+}
+
+add_action('admin_enqueue_scripts','firsttheme_assets');
