5f3f149f916f4f341ddb6767038c63be19a76679
Ankit Jindal <akjaph@gmail.com>
2018-10-25 15:22:51 +0530

    bundling-files-for-production

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,6 +10,7 @@ import webpack from 'webpack-stream';
 import uglify from 'gulp-uglify';
 import named from 'vinyl-named';
 import browserSync from 'browser-sync';
+import zip from 'gulp-zip';
 
 const server = browserSync.create();
 const PRODUCTION = yargs.argv.prod;
@@ -30,6 +31,21 @@ const paths = {
 	other: {
 		src: ['src/assets/**/*','!src/assets/{images,js,scss}', '!src/assets/{images,js,scss}/**/*'],
 		dest: 'dist/assets'
+	},
+	package: {
+		src: [
+		      "**/*",
+		      "!.vscode",
+		      "!node_modules{,/**}",
+		      "!packaged{,/**}",
+		      "!src{,/**}",
+		      "!.babelrc",
+		      "!.gitignore",
+		      "!gulpfile.babel.js",
+		      "!package.json",
+		      "!package-lock.json"
+    	],
+		dest: 'packaged'
 	}
 }
 
@@ -108,7 +124,14 @@ export const scripts = () => {
 	.pipe(gulp.dest(paths.scrips.dest));
 }
 
+export const compress = () => {
+	return gulp.src(paths.package.src)
+		.pipe(zip('firsttheme.zip'))
+		.pipe(gulp.dest(paths.package.dest));
+}
+
 export const dev = gulp.series(clean, gulp.parallel(styles, scripts, images, copy), serve, watch);
 export const build = gulp.series(clean, gulp.parallel(styles, scripts, images, copy));
+export const bundle = gulp.series(build,compress);
 
 export default dev;
