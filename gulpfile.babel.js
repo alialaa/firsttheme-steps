8c207374b8f4419794cea4db1a8a9890eb902de0
Ankit Jindal <akjaph@gmail.com>
2018-10-25 15:00:16 +0530

    improving-the-tasks-with-browsersync

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,9 +8,10 @@ import imagemin from 'gulp-imagemin';
 import del from 'del';
 import webpack from 'webpack-stream';
 import uglify from 'gulp-uglify';
-import named from 'vinyl-named'
+import named from 'vinyl-named';
+import browserSync from 'browser-sync';
 
-
+const server = browserSync.create();
 const PRODUCTION = yargs.argv.prod;
 
 const paths = {
@@ -32,6 +33,18 @@ const paths = {
 	}
 }
 
+export const serve = (done) => {
+	server.init({
+		proxy: "http://localhost/firsttheme/"
+	});
+	done();
+}
+
+export const reload = (done) => {
+	server.reload();
+	done();
+}
+
 export const clean = () => {
 	return del(['dist']);
 }
@@ -42,7 +55,8 @@ export const styles = (done) => {
 		.pipe(sass().on('error',sass.logError))
 		.pipe(gulpif(PRODUCTION, cleanCss({compatibility:'ie8'})))
 		.pipe(gulpif(!PRODUCTION, sourcemaps.write()))
-		.pipe(gulp.dest(paths.styles.dest));
+		.pipe(gulp.dest(paths.styles.dest))
+		.pipe(server.stream());
 }
 
 export const images = () => {
@@ -53,9 +67,10 @@ export const images = () => {
 
 export const watch = () => {
 	gulp.watch('src/assets/scss/**/*.scss', styles);
-	gulp.watch('src/assets/js/**/*.js', scripts);
-	gulp.watch(paths.images.src, images);
-	gulp.watch(paths.other.src, copy);
+	gulp.watch('src/assets/js/**/*.js', gulp.series(scripts, reload));
+	gulp.watch('**/*.php', reload);
+	gulp.watch(paths.images.src, gulp.series(images, reload));
+	gulp.watch(paths.other.src, gulp.series(copy, reload));
 } 
 
 
@@ -93,7 +108,7 @@ export const scripts = () => {
 	.pipe(gulp.dest(paths.scrips.dest));
 }
 
-export const dev = gulp.series(clean, gulp.parallel(styles, scripts, images, copy),watch);
+export const dev = gulp.series(clean, gulp.parallel(styles, scripts, images, copy), serve, watch);
 export const build = gulp.series(clean, gulp.parallel(styles, scripts, images, copy));
 
 export default dev;
