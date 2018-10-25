523bd03164f5f1545da26f3bbbe391a6000cd864
Ankit Jindal <akjaph@gmail.com>
2018-10-26 03:20:43 +0530

    customizer-overview-adding-new-file

--- functions.php
+++ functions.php
@@ -5,5 +5,6 @@ require_once('lib/enqueue-assets.php');
 require_once('lib/sidebars.php');
 require_once('lib/theme-support.php');
 require_once('lib/navigation.php');
+require_once('lib/customize.php');
 
 ?>
---
+++ lib/customize.php
@@ -0,0 +1,7 @@
+<?php 
+
+function _themename_customize_register( $wp_customize ) {
+
+}
+
+add_action( 'customize_register', '_themename_customize_register' );
