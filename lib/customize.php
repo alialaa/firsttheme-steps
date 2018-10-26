0799a1ec4a11eaed3f4965e5a628384e22c79127
Ankit Jindal <akjaph@gmail.com>
2018-10-26 15:31:46 +0530

    updating-using-selective-refresh

--- lib/customize.php
+++ lib/customize.php
@@ -4,6 +4,25 @@ function _themename_customize_register( $wp_customize ) {
 
 	$wp_customize->get_setting('blogname')->transport = 'postMessage';
 
+	$wp_customize->selective_refresh->add_partial('blogname', array(
+        // 'settings' => array('blogname')
+        'selector' => '.c-header__blogname',
+        'container_inclusive' => false,
+        'render_callback' => function() {
+            bloginfo( 'name' );
+        }
+    ));
+
+    $wp_customize->selective_refresh->add_partial('_themename_footer_partial', array(
+        'settings' => array('_themename_site_info'),
+        'selector' => '.c-site-info',
+        'container_inclusive' => true,
+        'render_callback' => function() {
+            get_template_part( 'template-parts/footer/info' );
+        }
+    ));
+
+
 	$wp_customize->add_section('_themename_footer_options', array(
         'title' => esc_html__( 'Footer Options', '_themename' ),
         'description' => esc_html__( 'You can change footer options from here.', '_themename' )
--- lib/theme-support.php
+++ lib/theme-support.php
@@ -4,6 +4,7 @@
 	    add_theme_support( 'title-tag' );
 	    add_theme_support( 'post-thumbnails' );
 		add_theme_support( 'html5', array('search-form', 'comment-list', 'comment-form', 'gallery', 'caption') );
+		add_theme_support( 'customize-selective-refresh-widgets' );
 	}
 
 	add_action( 'after_setup_theme', '_themename_theme_support' );
