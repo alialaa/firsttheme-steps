9957a689eff3eb24729d6d018289d1afcfb54db0
Ankit Jindal <akjaph@gmail.com>
2018-10-27 22:59:40 +0530

    displaying-the-comments-and-loading-necessary-js-files

--- lib/enqueue-assets.php
+++ lib/enqueue-assets.php
@@ -7,6 +7,10 @@ function _themename_assets() {
 	wp_add_inline_style( '_themename-stylesheet', $inline_styles );
 
 	wp_enqueue_script( '_themename-scripts', get_template_directory_uri() . '/dist/assets/js/bundle.js', array('jquery'), '1.0.0', true);
+
+	if (is_singular() && comments_open() && get_option( 'thread_comments' )) {
+		wp_enqueue_script( 'comment-reply' );
+	}
 }
 
 add_action('wp_enqueue_scripts','_themename_assets');
--- single.php
+++ single.php
@@ -10,24 +10,7 @@
     <div class="o-row">
         <div class="o-row__column o-row__column--span-12 o-row__column--span-<?php echo $layout === 'sidebar' ? '8' : '12' ?>@medium">
             <main role="main">
-                <?php if(have_posts()) { ?>
-                    <?php while(have_posts()) { ?>
-                        <?php the_post(); ?>
-
-                        <?php get_template_part('template-parts/post/content'); ?>
-                        <?php 
-                        if (get_theme_mod('_themename_display_author_info', true)) {
-                            get_template_part('template-parts/single/author');
-                        } 
-
-                        get_template_part('template-parts/single/navigation');
-
-                        ?>
-
-                    <?php } ?>
-                <?php } else { ?>
-                    <?php get_template_part('template-parts/post/content','none'); ?>
-                <?php } ?>
+                <?php get_template_part( 'loop', 'single' ) ?>
             </main>
         </div>
         <?php if( $layout === 'sidebar') { ?>
