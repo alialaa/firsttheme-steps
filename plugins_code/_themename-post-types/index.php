47cd222d325ccb3752470334d863480e69f4dbed
Ankit Jindal <akjaph@gmail.com>
2018-10-29 00:00:26 +0530

    syntax correction

--- plugins_code/_themename-post-types/index.php
+++ plugins_code/_themename-post-types/index.php
@@ -36,4 +36,4 @@ function _themename__pluginname_deactivate() {
     flush_rewrite_rules();
 }
 
-register_deactivation_hook(__FILE__, '_themename__pluginname_deactivate'
+register_deactivation_hook(__FILE__, '_themename__pluginname_deactivate');
