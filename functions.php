d9efb1e914f2773ae61ae7935005e161e23d650c
Ankit Jindal <akjaph@gmail.com>
2018-10-29 01:48:36 +0530

    flushing-rewrite-rules-after-saving-the-portfolio-slug-option

--- functions.php
+++ functions.php
@@ -21,9 +21,23 @@ function _themename_filter_portfolio($args, $post_type) {
 
 add_action('customize_save_after', '_themename_customize_save_after');
 
-function _themename_customize_save_after(){
-    flush_rewrite_rules();
+add_action('init', '_themename_flush_rewrite', 99999);
+
+function _themename_flush_rewrite() {
+    if(get_theme_mod('_themename_flush_flag', false)) {
+        flush_rewrite_rules();
+        set_theme_mod('_themename_flush_flag', false);
+    }
 }
+
+function _themename_customize_save_after() {
+    $old = get_post_type_object('_themename_portfolio')->rewrite['slug'];
+    $new = get_theme_mod('_themename_portfolio_slug', 'portfolio');
+    if($old !== $new) {
+        set_theme_mod('_themename_flush_flag', true);
+    }
+}
+
 
 
 if(!isset($content_width)) {
