c1832226df1ac1019311ab7e58ea00e64329d314
Ankit Jindal <akjaph@gmail.com>
2018-10-29 03:00:41 +0530

    creating-our-first-field-in-the-widget

--- lib/most-recent-widget.php
+++ lib/most-recent-widget.php
@@ -1,5 +1,4 @@
 <?php
-
 class _themename_Most_Recent_Widget extends WP_Widget {
 
     public function __construct() {
@@ -7,13 +6,27 @@ class _themename_Most_Recent_Widget extends WP_Widget {
             '_themename_mst_recent_widget',
             esc_html__('Recent Posts', '_themename'),
             array(
-                'description' => esc_html__('some description', '_themename')
+                'description' => esc_html__('some description', '_themename'),
+                'customize_selective_refresh' => true
             )
         );
     }
 
     public function form( $instance ) {
-        echo "sldjlkj";
+        var_dump($instance);
+        if (isset($instance['title'])) {
+            $title = $instance['title'];
+        }
+        else {
+            $title = "";
+        }
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_id('title') ?>"><?php esc_html_e('Title:', '_themename'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('title') ?>" name="<?php echo $this->get_field_name('title') ?>" type="text" value="<?php echo esc_attr($title); ?>" />
+        </p>
+
+        <?php
     }
 
     public function widget($args, $instance) {
@@ -22,7 +35,9 @@ class _themename_Most_Recent_Widget extends WP_Widget {
     }
 
     public function update($new_instance, $old_instance) {
-    
+        $instance = array();
+        $instance['title'] = isset($new_instance['title']) ? sanitize_text_field($new_instance['title']) : '';
+        return $instance;
     }
 }
 
