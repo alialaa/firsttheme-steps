155ad405baabd8807ac325e608b469b024586cee
Ankit Jindal <akjaph@gmail.com>
2018-10-29 03:36:25 +0530

    sanitizing-and-displaying-our-fields

--- lib/most-recent-widget.php
+++ lib/most-recent-widget.php
@@ -1,4 +1,13 @@
 <?php
+
+function _themename_sanitize_sort_by( $input ) {
+    $valid = array('date', 'rand', 'comment_count');
+    if( in_array($input, $valid, true) ) {
+        return $input;
+    }
+    return 'date';
+}
+
 class _themename_Most_Recent_Widget extends WP_Widget {
 
     public function __construct() {
@@ -13,13 +22,30 @@ class _themename_Most_Recent_Widget extends WP_Widget {
     }
 
     public function form( $instance ) {
-        var_dump($instance);
-        if (isset($instance['title'])) {
+        if(isset($instance['title'])) {
             $title = $instance['title'];
-        }
-        else {
+        } else {
             $title = "default";
         }
+
+        if(isset($instance['post_count'])) {
+            $post_count = $instance['post_count'];
+        } else {
+            $post_count = 3;
+        }
+
+        if(isset($instance['include_date'])) {
+            $include_date = $instance['include_date'];
+        } else {
+            $include_date = false;
+        }
+
+        if(isset($instance['sort_by'])) {
+            $sort_by = $instance['sort_by'];
+        } else {
+            $sort_by = 'date';
+        }
+
         ?>
         <p>
             <label for="<?php echo $this->get_field_id('title') ?>"><?php esc_html_e('Title:', '_themename'); ?></label>
@@ -27,18 +53,18 @@ class _themename_Most_Recent_Widget extends WP_Widget {
         </p>
         <p>
             <label for="<?php echo $this->get_field_id('post_count') ?>"><?php esc_html_e('Number Of Posts:', '_themename'); ?></label>
-            <input class="widefat" id="<?php echo $this->get_field_id('post_count') ?>" name="<?php echo $this->get_field_name('post_count') ?>" type="number" min="1" />
+            <input class="widefat" id="<?php echo $this->get_field_id('post_count') ?>" name="<?php echo $this->get_field_name('post_count') ?>" type="number" min="1" value="<?php echo intval($post_count); ?>" />
         </p>
         <p>
-            <input type="checkbox" id="<?php echo $this->get_field_id('include_date') ?>" name="<?php echo $this->get_field_name('include_date') ?>" />
+            <input <?php checked($include_date); ?> type="checkbox" id="<?php echo $this->get_field_id('include_date') ?>" name="<?php echo $this->get_field_name('include_date') ?>" />
             <label for="<?php echo $this->get_field_id('include_date') ?>"><?php esc_html_e('Include Date?', '_themename'); ?></label>
         </p>
         <p>
             <label for="<?php echo $this->get_field_id('sort_by') ?>"><?php esc_html_e('Sort By:', '_themename'); ?></label>
             <select class="widefat" id="<?php echo $this->get_field_id('sort_by') ?>" name="<?php echo $this->get_field_name('sort_by') ?>">
-                <option value="date"><?php esc_html_e('Most Recent', "_themename") ?></option>
-                <option value="rand"><?php esc_html_e('Random', "_themename") ?></option>
-                <option value="comment_count"><?php esc_html_e('Number Of Comments', "_themename") ?></option>
+                <option <?php selected($sort_by, 'date'); ?> value="date"><?php esc_html_e('Most Recent', "_themename") ?></option>
+                <option <?php selected($sort_by, 'rand'); ?> value="rand"><?php esc_html_e('Random', "_themename") ?></option>
+                <option <?php selected($sort_by, 'comment_count'); ?> value="comment_count"><?php esc_html_e('Number Of Comments', "_themename") ?></option>
             </select>
         </p>
 
@@ -53,6 +79,9 @@ class _themename_Most_Recent_Widget extends WP_Widget {
     public function update($new_instance, $old_instance) {
         $instance = array();
         $instance['title'] =  sanitize_text_field($new_instance['title']);
+        $instance['post_count'] = intval($new_instance['post_count']);
+        $instance['include_date'] = boolval($new_instance['include_date']);
+        $instance['sort_by'] = _themename_sanitize_sort_by($new_instance['sort_by']);
         return $instance;
     }
 }
